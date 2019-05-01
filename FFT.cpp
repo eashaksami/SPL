bf7589aeb716a29f2cf44d9cadf896714ced81e1
eashaksami <46522742+eashaksami@users.noreply.github.com>
2019-05-01 07:55:05 +0600

    Update FFT.cpp

--- FFT.cpp
+++ FFT.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 #include<complex>
 #include<math.h>
+#include<vector>
 using namespace std;
 
 typedef complex<double> Complex;
 int N;
 
-Complex Exponent(double a)
+complex<float> Exponent(double a)
 {
-    Complex b;
+    complex<float> b;
 
     if(a==0||((int)a%180==0&&(int)a%360==0))
     {
@@ -28,8 +29,8 @@ Complex Exponent(double a)
     	x=a/90;
     	if((((x-1)/2)%2)==0)
     	{
-    	b.real(0.0);
-    	b.imag(1.0);
+            b.real(0.0);
+            b.imag(1.0);
     	}
     	else
         {
@@ -42,42 +43,38 @@ Complex Exponent(double a)
     {
         double c,d;
         b.real(cos((a*M_PI)/180));
-    	b.imag(sin((a*M_PI)/180));
+        b.imag(sin((a*M_PI)/180 ));
     }
 
     return b;
 }
 
-Complex* fft(Complex X[])
+vector<complex<float> > fft(vector<complex<float> >X)
 {
-    Complex a(0.0,0.0),b(0.0,0.0),c,d,e;
-    Complex *Y = new Complex[N];
+     complex<float> a(0.0,0.0),b(0.0,0.0),c,d,e;
+     vector<complex<float> > Y(N);
+
     for(int k = 0;k<N/2;k++)
     {
         d = Exponent((2*180*k)/(N));
         for(int n=0;n<N/2;n++)
         {
-            //c = exponent((2*M_PI*k*n)/(N));
-
             c = Exponent((4*180*k*n)/(N));
-            //d = exponent((2*M_PI*k)/(N));
-
             a += X[2*n]*c;
             e = X[(2*n)+1]*c;
             b += e*d;
         }
         Y[k] = a+b;
         Y[k+N/2]=a-b;
-		a=(0.0,0.0);
-		b=(0.0,0.0);
+    	a.real(0.0);
+		a.imag(0.0);
+		b.real(0.0);
+		b.imag(0.0);
     }
-
-    Complex z;
-    //z=cos((90*M_PI)/180),sin((90*M_PI)/180);
-    //cout<<cos((90*M_PI)/180);
-
     return Y;
 }
+
+
 
 int main(void)
 {
@@ -96,31 +93,35 @@ int main(void)
             cout<<"Enter 2^N points: ";
             cin>>N;
         }
-
     }
+
     cout<<"Enter the points:"<<endl;
-    //Complex a[N];
+
     double a[N];
-    Complex x[N];
+    int c;
+    vector<complex<float> > x;
     if(n==1)
     {
         for(int i=0;i<N;i++)
         {
-            cin>>a[i];
-            x[i].real(a[i]);
-            x[i].imag(0);
+            cin>>c;
+            x.push_back( complex<float>( c, 0 ) );
         }
+
     }
     else
     {
         for(int i=0;i<N;i++)
             cin>>x[i].real()>>x[i].imag();
     }
-
-    Complex *b = new Complex[N];
+    vector<complex<float> > b(N);
         b=fft(x);
         for(int i=0;i<N;i++)
             cout<<b[i]<<endl;
-            //cout<<M_PI<<endl;
-        //cout<<sin(150*M_PI/180)<<endl;
+
 }
+
+
+
+
+
